9e0c81817e7c8d7c24fa3340dd28f55ab57ca299
Carlos Wimmer <carloswimmer@santos.sp.gov.br>
2019-11-26 15:31:37 -0300

    Initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat consectetur iste illo quo, laboriosam dolorum maiores quos perspiciatis officia soluta quas doloribus voluptatem rerum error dignissimos, pariatur excepturi dicta tenetur!</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller Display</h4>
+    <p class="headline">This is headline</p>
+    <p class="subheadline">This is subheadline</p>
+    <p class="caption">This is caption</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  name: 'home'
 }
 </script>
