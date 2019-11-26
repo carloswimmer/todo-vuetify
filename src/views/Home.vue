eede1a6649edb88db0160a672817f0f5316cf8df
Carlos Wimmer <carloswimmer@santos.sp.gov.br>
2019-11-26 16:31:52 -0300

    Examples of buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,25 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat consectetur iste illo quo, laboriosam dolorum maiores quos perspiciatis officia soluta quas doloribus voluptatem rerum error dignissimos, pariatur excepturi dicta tenetur!</p>
-    <h1 class="display-4">Massive Display</h1>
-    <h4 class="display-1">Smaller Display</h4>
-    <p class="headline">This is headline</p>
-    <p class="subheadline">This is subheadline</p>
-    <p class="caption">This is caption</p>
+    
+      <v-btn class="pink mx-3" dark>click me</v-btn>
+      <v-btn depressed class="pink mx-3">click me</v-btn>
+      <v-btn text color="pink" class="mx-3">click me</v-btn>
+      <v-btn depressed class="pink mx-3" dark>
+        <v-icon left>mdi-email</v-icon>
+        email
+      </v-btn>
+      <v-btn small class="pink mx-3" dark>
+        <v-icon small left>mdi-email</v-icon>
+        email
+      </v-btn>
+      <v-btn large depressed class="pink mx-3" dark>
+        email
+        <v-icon right>mdi-email</v-icon>
+      </v-btn>
+      <v-btn fab small depressed class="purple mx-3" dark>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
   </div>
 </template>
 
@@ -15,3 +28,9 @@ export default {
   name: 'home'
 }
 </script>
+
+<style>
+.home {
+  margin: 15px;
+}
+</style>
