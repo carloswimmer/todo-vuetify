b69cac7b0fbed1ea510a9b3745f2c3fa927e648f
Carlos Wimmer <carloswimmer@santos.sp.gov.br>
2019-12-03 15:41:18 -0300

    Create an issue card

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,28 +2,26 @@
   <div>
     <v-subheader>Dashboard</v-subheader>
     <v-container class="my-5">
-      <v-row  >
-        <v-col cols="12" md="6">
-          <v-btn outlined block color="primary">1</v-btn>
-        </v-col>
-        <v-col cols="4" md="2">
-          <v-btn outlined block color="primary">2</v-btn>
-        </v-col>
-        <v-col cols="4" md="2">
-          <v-btn outlined block color="primary">2</v-btn>
-        </v-col>
-        <v-col cols="4" md="2">
-          <v-btn outlined block color="primary">2</v-btn>
-        </v-col>
-      </v-row>
-      <v-row class="justify-space-around">
-        <v-col cols="4" md="3">
-          <v-btn outlined block color="success">1</v-btn>
-        </v-col>
-        <v-col cols="4" md="3">
-          <v-btn outlined block color="success">2</v-btn>
-        </v-col>
-      </v-row>
+      <v-card class="px-3">
+        <v-row>
+          <v-col cols="12" md="6">
+            <div class="caption grey--text">Project title</div>
+            <div>Create a new website</div>
+          </v-col>
+          <v-col cols="6" sm="4" md="2">
+            <div class="caption grey--text">Person</div>
+            <div>Carlos Wimmer</div>
+          </v-col>
+          <v-col cols="6" sm="4" md="2">
+            <div class="caption grey--text">Due by</div>
+            <div>1st May 2019</div>
+          </v-col>
+          <v-col cols="2" sm="4" md="2">
+            <div class="caption grey--text">Status</div>
+            <div>ongoing</div>
+          </v-col>
+        </v-row>
+      </v-card>
     </v-container>
   </div>
 </template>
