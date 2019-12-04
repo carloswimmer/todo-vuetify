6eb84746073f0a1e74107f6b6bab81ca304c7e9c
Carlos Wimmer <carloswimmer@santos.sp.gov.br>
2019-12-04 18:44:10 -0300

    Include expansion panel on My Projects

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,14 +1,41 @@
 <template>
   <div>
     <v-subheader>Projects</v-subheader>
-    <v-container class="ma-5">
-      content
+    <v-container class="my-5">
+      <v-expansion-panels>
+        <v-expansion-panel>
+          <v-expansion-panel-header>
+            project title
+          </v-expansion-panel-header>
+          <v-expansion-panel-content class="px-4 grey--text">
+            <div class="font-weight-bold">
+              due by xx.xx.xx
+            </div>
+            <div>info</div>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </v-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'projects'
+  name: 'projects',
+  data() {
+    return {
+      projects: [
+        { title: 'Design a new website', person: 'Carlos Wimmer', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quis possimus unde nihil illum harum maxime ex? Sed, dolor! Odio placeat iure totam voluptatum accusamus unde excepturi' },
+        { title: 'Code up the homepage', person: 'Marcelo Lopes', due: '10th Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quis possimus unde nihil illum harum maxime ex? Sed, dolor! Odio placeat iure totam voluptatum accusamus unde excepturi repudiandae numquam?' },
+        { title: 'Design video thumbnails', person: 'Edmar Hilário', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quis possimus unde nihil illum harum maxime ex? Sed, dolor! Odio placeat iure totam voluptatum accusamus unde Perferendis?' },
+        { title: 'Create a community forum', person: 'Luiz Teixeira', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum quis possimus unde nihil illum harum maxime ex? Sed, dolor! Odio placeat iure totam voluptatum accusamus unde excepturi repudiandae numquam? Perferendis?' },
+      ]
+    }
+  },
+  computed: {
+    myProjects () {
+      return this.projects.filter(item => item.person === 'Carlos Wimmer')
+    }
+  }
 }
 </script>
