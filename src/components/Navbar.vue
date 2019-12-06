5686cc9fb4abc6c8ff41e007398d8dff672aef5e
Carlos Wimmer <carloswimmer@santos.sp.gov.br>
2019-12-06 11:33:54 -0300

    Include menu on Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,16 +2,36 @@
   <nav>
     <v-app-bar app clipped-left>
       <v-app-bar-nav-icon class="grey--text" @click="drawer = !drawer"></v-app-bar-nav-icon>
+
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Todo</span>
         <span>Ninja</span>
       </v-toolbar-title>
+
       <v-spacer></v-spacer>
+
+      <div class="text-center">
+        <v-menu offset-y>
+          <template v-slot:activator="{ on }">
+            <v-btn text color="grey" dark v-on="on">
+              <v-icon left>mdi-chevron-down</v-icon>
+              Menu
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="(item, index) in items" :key="index" router :to="item.route">
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+
       <v-btn text color="grey">
         <span>Sign Out</span>
         <v-icon right>mdi-exit-to-app</v-icon>
       </v-btn>
     </v-app-bar>
+
     <v-navigation-drawer dark clipped app v-model="drawer" color="#00b2cc">
       <v-list>
         <v-list-item>
@@ -25,16 +45,13 @@
             <v-list-item-subtitle>cw@santos.sp.gov.br</v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
-        <v-list-item v-for="item in items" :key="item.title" router :to="item.route" >
+
+        <v-list-item v-for="item in items" :key="item.title" router :to="item.route">
           <v-list-item-icon>
-            <v-icon>
-              {{ item.icon }}
-            </v-icon>
+            <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
           <v-list-item-content>
-            <v-list-item-title>
-              {{ item.title }}
-            </v-list-item-title>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
